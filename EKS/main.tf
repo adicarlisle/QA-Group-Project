74e8d0fb63160afcccb1f599e91dc1248e987dc8
Ubuntu <ubuntu@ip-172-31-29-252.eu-west-2.compute.internal>
2021-08-10 13:49:38 +0000

    Avoiding use of t3.medium

--- EKS/main.tf
+++ EKS/main.tf
@@ -18,6 +18,8 @@ resource "aws_eks_node_group" "qa_node_group" {
 
   node_group_name = "qa_node_group"
 
+  instance_types = ["t2.micro"]
+
   node_role_arn = var.qa_node_iam_role_arn
 
   subnet_ids = var.subnet_ids
@@ -26,9 +28,9 @@ resource "aws_eks_node_group" "qa_node_group" {
 
     desired_size = 1
 
-    max_size = 1
+    max_size = 5
 
-    min_size = 1
+    min_size = 3
 
   }
 
