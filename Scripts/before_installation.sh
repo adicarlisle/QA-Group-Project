92b84392e4575812c7b2829f6847c7fcecb74aa7
Ubuntu <ubuntu@ip-172-31-2-208.eu-west-2.compute.internal>
2021-08-11 09:20:38 +0000

    Scripts

---
+++ Scripts/before_installation.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+error()
+{
+    echo "[ERROR]:" "$1" "EXITING" 1>&2
+    exit 1
+}
+
+warning()
+{
+    echo "[WARNING]:" "$1" 1>&2
+}
+
+output()
+{
+    echo "[OUTPUT]:" "$1" 1>&2
+}
+
+output "Updating to make sure the latest security patches are installed"
+sudo apt update -y
+
+if [[ "$(aws --version 2> /dev/null)" == "" ]]; then
+    error "AWS CLI is not installed"
+fi
+
+
+aws eks update-kubeconfig --name qa_cluster
