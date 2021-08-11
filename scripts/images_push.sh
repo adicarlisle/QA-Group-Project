a9d10dc2d312216be7c7353d0bd39a7fc139e16d
Ubuntu <ubuntu@ip-172-31-29-252.eu-west-2.compute.internal>
2021-08-11 11:41:57 +0000

    Docker push changes

--- scripts/images_push.sh
+++ scripts/images_push.sh
@@ -20,19 +20,19 @@ output "Pushing acarli/team_2_angularpet to dockerhub"
 
 if [[ ! "$(docker images -q acarli/team_2_angularpet:latest 2> /dev/null)" == "" ]]; then
     output "Image exists"
-    docker push qa/petclinic-fe
+    docker push acarli/team_2_angularpet
 fi
 
 output "Pushing acarli/team_2_javapet to dockerhub"
 
 if [[ ! "$(docker images -q acarli/team_2_javapet:latest 2> /dev/null)" == "" ]]; then
     output "Image exists"
-    docker push qa/petclinic-be
+    docker push acarli/team_2_javapet
 fi
 
-output "Pushing qa/nginx to dockerhub"
+output "Pushing acarli/nginx to dockerhub"
 
 if [[ ! "$(docker images -q qa/nginx:latest 2> /dev/null)" == "" ]]; then
     output "Image exists"
-    docker push qa/nginx
+    docker push acarli/nginx
 fi
